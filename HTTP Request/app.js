228644daab466a4dafd1fcb899082ac5b539af87
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-01-20 22:09:53 -0300

    Agrego comments de explicacion

--- HTTP Request/app.js
+++ HTTP Request/app.js
@@ -1,3 +1,4 @@
+
 fetch("https://swapi.co/api/planets/")
     .then((response) => {
         if (!response.ok)
@@ -7,18 +8,23 @@ fetch("https://swapi.co/api/planets/")
     })
     .then((data) => { //llamo al metodo json para acceder a data
         console.log("Fetched all planets (first 10)");
-        const filmURL = data.results[0].films[0]; //accediendo al primer film del primer planet
-        return fetch(filmURL) //Promise, la cual puedo return
+        for (let planet of data.results) {
+            console.log(planet.name);
+        }
+        const nextURL = data.next; //La API tiene una propiedad llamada next que permite acceder a la siguiente pagina de data
+        return fetch(nextURL) //Promise, la cual puedo return
     })
     .then((response) => {
-        if (!response.ok)
+        if (!response.ok) // chequea si esta todo correcto para fetch
             throw new Error(`Status Code Error: ${response.status}`);
 
-        return response.json();
+        return response.json(); //si lo esta, aplica el metodo json(convertir)
     })
     .then((data) => {
-        console.log("Fetched first film from first planet");
-        console.log(data.title);
+        console.log("Fetched Next 10 Planets");
+        for (let planet of data.results) {
+            console.log(planet.name);
+        };
     })
     .catch((err) => {
         console.log("Something went wrong with fetch");
