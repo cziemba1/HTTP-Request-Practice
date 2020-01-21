25ac7c76a0f7a5e81fd43352716af7e7f99be96c
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-01-20 22:00:19 -0300

    Chaining fetch request

--- HTTP Request/app.js
+++ HTTP Request/app.js
@@ -3,13 +3,24 @@ fetch("https://swapi.co/api/planets/")
         if (!response.ok)
             throw new Error(`Status Code Error: ${response.status}`);
 
-        response.json().then((data) => { //llamo al metodo json para acceder a data
-            for (let planet of data.results) {
-                console.log(planet.name);
-            }
-        });
+        return response.json();
+    })
+    .then((data) => { //llamo al metodo json para acceder a data
+        console.log("Fetched all planets (first 10)");
+        const filmURL = data.results[0].films[0]; //accediendo al primer film del primer planet
+        return fetch(filmURL) //Promise, la cual puedo return
+    })
+    .then((response) => {
+        if (!response.ok)
+            throw new Error(`Status Code Error: ${response.status}`);
+
+        return response.json();
+    })
+    .then((data) => {
+        console.log("Fetched first film from first planet");
+        console.log(data.title);
     })
     .catch((err) => {
         console.log("Something went wrong with fetch");
         console.log(err);
-    });
+    })
